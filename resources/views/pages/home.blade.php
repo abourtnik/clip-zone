e722b931f15f208226a46e2463330b4e41d358e6
abourtnik <contact@antonbourtnik.fr>
2023-04-24 17:37:55 +0200

    Add responsive margin home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Share and Watch Amazing Videos')
 
-@section('class', 'px-0 px-sm-2')
+@section('class', 'px-0 px-sm-2 mt-0 mt-sm-3')
 
 @section('content')
     <videos-area url="{{route('videos.home')}}" />
