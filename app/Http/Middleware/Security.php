68ecd0e877cbefed7ef880e7c3df4a5e399c6843
abourtnik <contact@antonbourtnik.fr>
2023-09-12 23:45:04 +0200

    fix : test set header manually

--- app/Http/Middleware/Security.php
+++ app/Http/Middleware/Security.php
@@ -25,7 +25,6 @@ class Security
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
-        $response->headers->set('Content-Security-Policy', "default-src 'self' http: https: data: blob: 'unsafe-inline'");
         $response->headers->set('Strict-Transport-Security', "max-age=31536000; includeSubDomains");
 
         foreach ($this->exceptIframe as $except) {
