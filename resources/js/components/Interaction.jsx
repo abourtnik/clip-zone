865e76aa3c750cd172cbc268b2f6b40bd74e3c8d
abourtnik <contact@antonbourtnik.fr>
2023-10-03 13:33:26 +0200

    ref : add useReducer for Interaction component

--- resources/js/components/Interaction.jsx
+++ resources/js/components/Interaction.jsx
@@ -1,29 +1,44 @@
-import { useState } from 'preact/hooks';
+import { useReducer } from 'preact/hooks';
 import {jsonFetch} from '../hooks'
+
+const reducer = (state, action) => {
+    switch (action) {
+        case 'LIKE':
+            return {
+                ...state,
+                liked: !state.liked,
+                disliked : false,
+                counterLike: state.liked ? state.counterLike - 1 : state.counterLike + 1,
+                counterDislike: state.disliked ? state.counterDislike - 1 : state.counterDislike
+            }
+            break;
+        case 'DISLIKE':
+            return {
+                ...state,
+                disliked: !state.disliked,
+                liked : false,
+                counterDislike: state.disliked ? state.counterDislike - 1 : state.counterDislike + 1,
+                counterLike: state.liked ? state.counterLike - 1 : state.counterLike
+            }
+            break;
+    }
+}
 
 export default function Interaction ({model, target, count, active, showCount = true}) {
 
     const {like, dislike} = JSON.parse(active)
     const {likes_count, dislikes_count} = JSON.parse(count)
 
-    const [liked, setLiked] = useState(like);
-    const [disliked, setDisLiked] = useState(dislike);
-    const [counterLike, setCounterLike] = useState(likes_count);
-    const [counterDislike, setCounterDislike] = useState(dislikes_count);
+    const [state, dispatch] = useReducer(reducer, {
+        liked: like,
+        disliked : dislike,
+        counterLike : likes_count,
+        counterDislike : dislikes_count
+    })
 
     const handleClick = async (type) => {
 
-        if (type === 'like') {
-            setLiked(liked => !liked)
-            setDisLiked(false)
-            liked ? setCounterLike(counter => counter - 1) : setCounterLike(counter => counter + 1);
-            disliked ? setCounterDislike(counter => counter - 1) : null;
-        } else {
-            setDisLiked(liked => !liked)
-            setLiked(false)
-            disliked ? setCounterDislike(counter => counter - 1) : setCounterDislike(counter => counter + 1);
-            liked ? setCounterLike(counter => counter - 1) : null;
-        }
+        dispatch(type.toUpperCase())
 
         await jsonFetch(`/api/${type}` , {
             method: 'POST',
@@ -38,29 +53,29 @@ export default function Interaction ({model, target, count, active, showCount = 
         <div className={'d-flex justify-content-between bg-light-dark rounded-4'}>
             <button
                 onClick={() => handleClick('like')}
-                className={'hover-grey btn btn-sm border border-0 px-3 rounded-5 rounded-end ' + (liked ? 'text-success' : 'text-black') + (!showCount || counterLike === 0 ? ' py-2' : '')}
+                className={'hover-grey btn btn-sm border border-0 px-3 rounded-5 rounded-end ' + (state.liked ? 'text-success' : 'text-black') + (!showCount || state.counterLike === 0 ? ' py-2' : '')}
                 data-bs-toggle="tooltip"
                 data-bs-title="I like this"
                 data-bs-placement="bottom"
                 data-bs-trigger="hover"
             >
                 <div className={'d-flex gap-1 align-items-center'}>
-                    {liked ? <i className="fa-solid fa-thumbs-up"></i> : <i className="fa-regular fa-thumbs-up"></i>}
-                    { (showCount && counterLike > 0) && <span className={'ml-1'}>{counterLike}</span>}
+                    {state.liked ? <i className="fa-solid fa-thumbs-up"></i> : <i className="fa-regular fa-thumbs-up"></i>}
+                    { (showCount && state.counterLike > 0) && <span className={'ml-1'}>{state.counterLike}</span>}
                 </div>
             </button>
             <div className="vr h-75 my-auto"></div>
             <button
                 onClick={() => handleClick('dislike')}
-                className={'hover-grey btn btn-sm border border-0 px-3 rounded-5 rounded-start ' + (disliked ? 'text-danger' : 'text-black')}
+                className={'hover-grey btn btn-sm border border-0 px-3 rounded-5 rounded-start ' + (state.disliked ? 'text-danger' : 'text-black')}
                 data-bs-toggle="tooltip"
                 data-bs-title="I dislike this"
                 data-bs-placement="bottom"
                 data-bs-trigger="hover"
             >
                 <div className={'d-flex gap-1  align-items-center'}>
-                    {disliked ? <i className="fa-solid fa-thumbs-down"></i> : <i className="fa-regular fa-thumbs-down"></i>}
-                    { (showCount && counterDislike > 0) && <span className={'ml-1'}>{counterDislike}</span>}
+                    {state.disliked ? <i className="fa-solid fa-thumbs-down"></i> : <i className="fa-regular fa-thumbs-down"></i>}
+                    { (showCount && state.counterDislike > 0) && <span className={'ml-1'}>{state.counterDislike}</span>}
                 </div>
             </button>
         </div>
