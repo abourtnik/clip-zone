896602c105249fb575f35bdaad4e84d4819a0e22
abourtnik <contact@antonbourtnik.fr>
2023-12-04 16:20:41 +0100

    feat: update premium plan

--- config/plans.php
+++ config/plans.php
@@ -3,12 +3,12 @@
 return [
     'free' => [
         'max_uploads' => 10,
-        'max_file_size' => 2209715200, // 200 MB
-        'max_videos_storage' => 5073741824 // 1 GB
+        'max_file_size' => 524288000, // 500 MB
+        'max_videos_storage' => 5368709120 // 5 GB
     ],
     'premium' => [
         'max_uploads' => null,
-        'max_file_size' => 2073741824, // 1 GB
+        'max_file_size' => 2147483648, // 2 GB
         'max_videos_storage' => null
     ],
     'trial_period' => [
