c1399afea7b6eb24ade9e51673bb838555c1c69f
abourtnik <contact@antonbourtnik.fr>
2023-12-07 18:07:55 +0100

    fix: adapt api RateLimiter

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
@@ -46,11 +47,24 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting() : void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip())->response(function (Request $request, array $headers) {
-                return response()->json([
-                    'message' => 'We have received too many requests, please wait before renewing your action'
-                ], 429);
-            });
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip())
+                ->response(fn(Request $request, array $headers) => $this->response($request, $headers));
         });
+
+        RateLimiter::for('subscribe', function (Request $request) {
+            return Limit::perMinute(5)->by($request->user()->id . $request->user)
+                ->response(fn(Request $request, array $headers) => $this->response($request, $headers));
+        });
+
+        RateLimiter::for('upload', function (Request $request) {
+            return Limit::perMinute(70)->by($request->user()->id . $request->get('resumableIdentifier'))
+                ->response(fn(Request $request, array $headers) => $this->response($request, $headers));
+        });
+    }
+
+    private function response (Request $request, array $headers): JsonResponse {
+        return response()->json([
+            'message' => 'We have received too many requests, please wait before renewing your action'
+        ], 429);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -34,53 +34,59 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post("subscribe/{user}", [ProfileController::class, 'subscribe'])
         ->name('subscribe')
         ->can('subscribe', 'user')
+        ->middleware('throttle:subscribe')
         ->missing(fn() => abort(404, 'User not found'));
-
-    // INTERACTIONS
-    Route::name('interactions.')->controller(InteractionController::class)->group(function () {
-        Route::get('/interactions', 'list')->name('list');
-        Route::post('/like', 'like')->name('like');
-        Route::post('/dislike', 'dislike')->name('dislike');
-    });
-
-    // COMMENTS
-    Route::prefix('comments')->name('comments.')->controller(CommentController::class)->group(function () {
-        Route::get('/{comment}/replies', 'replies')->name('replies');
-        Route::post('/', 'store')->name('store');
-        Route::put('/{comment}', 'update')->name('update')->can('update', 'comment');
-        Route::delete('/{comment}', 'delete')->name('delete')->can('delete', 'comment');
-        Route::post('/{comment}/pin', 'pin')->name('pin')->can('pin', 'comment');
-        Route::post('/{comment}/unpin', 'unpin')->name('unpin')->can('pin', 'comment');
-    });
 
     // UPLOAD
     Route::post('/videos/upload', [VideoUserController::class, 'upload'])
         ->name('videos.upload')
+        ->middleware('throttle:upload')
         ->can('upload', Video::class);
 
-    // REPORT
-    Route::post('/report', [ReportController::class, 'report'])->name('report');
+    Route::middleware('throttle:api')->group(function () {
 
-    // PLAYLIST
-    Route::controller(PlaylistController::class)->prefix('playlists')->name('playlists.')->group(function () {
-        Route::get('/{video}', 'list')->name('list');
-        Route::post('/save', 'save')->name('save');
-    });
+        // INTERACTIONS
+        Route::name('interactions.')->controller(InteractionController::class)->group(function () {
+            Route::get('/interactions', 'list')->name('list');
+            Route::post('/like', 'like')->name('like');
+            Route::post('/dislike', 'dislike')->name('dislike');
+        });
 
-    // NOTIFICATIONS
-    Route::controller(NotificationController::class)->prefix('notifications')->name('notifications.')->group(function () {
-        Route::get('/{notification}/read', 'read')
-            ->name('read')
-            ->missing(fn() => abort(404, 'Notification not found'))
-            ->can('read', 'notification');
-        Route::get('/{notification}/unread', 'unread')->name('unread')
-            ->missing(fn() => abort(404, 'Notification not found'))
-            ->can('unread', 'notification');
-        Route::delete('/{notification}/delete', 'delete')
-            ->name('remove')
-            ->missing(fn() => abort(404, 'Notification not found'))
-            ->can('delete', 'notification');
-        Route::get('/read-all', 'readAll')->name('read-all');
+        // COMMENTS
+        Route::prefix('comments')->name('comments.')
+            ->controller(CommentController::class)->group(function () {
+                Route::get('/{comment}/replies', 'replies')->name('replies');
+                Route::post('/', 'store')->name('store');
+                Route::put('/{comment}', 'update')->name('update')->can('update', 'comment');
+                Route::delete('/{comment}', 'delete')->name('delete')->can('delete', 'comment');
+                Route::post('/{comment}/pin', 'pin')->name('pin')->can('pin', 'comment');
+                Route::post('/{comment}/unpin', 'unpin')->name('unpin')->can('pin', 'comment');
+            });
+
+        // REPORT
+        Route::post('/report', [ReportController::class, 'report'])->name('report');
+
+        // PLAYLIST
+        Route::controller(PlaylistController::class)->prefix('playlists')->name('playlists.')->group(function () {
+            Route::get('/{video}', 'list')->name('list');
+            Route::post('/save', 'save')->name('save');
+        });
+
+        // NOTIFICATIONS
+        Route::controller(NotificationController::class)->prefix('notifications')->name('notifications.')->group(function () {
+            Route::get('/{notification}/read', 'read')
+                ->name('read')
+                ->missing(fn() => abort(404, 'Notification not found'))
+                ->can('read', 'notification');
+            Route::get('/{notification}/unread', 'unread')->name('unread')
+                ->missing(fn() => abort(404, 'Notification not found'))
+                ->can('unread', 'notification');
+            Route::delete('/{notification}/delete', 'delete')
+                ->name('remove')
+                ->missing(fn() => abort(404, 'Notification not found'))
+                ->can('delete', 'notification');
+            Route::get('/read-all', 'readAll')->name('read-all');
+        });
     });
 
 });
@@ -90,17 +96,21 @@ Route::middleware('auth:sanctum')->group(function () {
  * -------------------- PUBLIC --------------------
 */
 
-// SEARCH
-Route::get("search", [SearchController::class, 'search'])->name('search');
-Route::post("search-videos", [SearchController::class, 'searchVideos'])->name('search-videos');
+Route::middleware('throttle:api')->group(function () {
+
+    // SEARCH
+    Route::get("search", [SearchController::class, 'search'])->name('search');
+    Route::post("search-videos", [SearchController::class, 'searchVideos'])->name('search-videos');
 
 // VIDEOS
-Route::prefix('videos')->name('videos.')->controller(VideoController::class)->group(function () {
-    Route::get('/home', 'home')->name('home');
-    Route::get('/trend', 'trend')->name('trend');
-    Route::get('/category/{category}', 'category')->name('category');
-    Route::get('/user/{user}', 'user')->name('user');
-});
+    Route::prefix('videos')->name('videos.')->controller(VideoController::class)->group(function () {
+        Route::get('/home', 'home')->name('home');
+        Route::get('/trend', 'trend')->name('trend');
+        Route::get('/category/{category}', 'category')->name('category');
+        Route::get('/user/{user}', 'user')->name('user');
+    });
 
 // COMMENTS
-Route::get("comments", [CommentController::class, 'list'])->name('comments.list');
+    Route::get("comments", [CommentController::class, 'list'])->name('comments.list');
+
+});
