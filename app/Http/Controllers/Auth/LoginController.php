5a41c758ec3eadfe4f7440573dd9829d83549406
abourtnik <contact@antonbourtnik.fr>
2023-12-11 18:05:49 +0100

    feat: test ci failed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Hash;
 class LoginController
 {
     public function show(): View {
-        return view('auth.login');
+        return view('auth.login2');
     }
 
     public function login (Request $request) : RedirectResponse {
