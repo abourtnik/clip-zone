4b8b80c68a6214842767de96981f9b2f1ddb6c22
abourtnik <contact@antonbourtnik.fr>
2023-12-28 16:12:29 +0100

    fix: eager loading interactions

--- app/Http/Controllers/User/VideoController.php
+++ app/Http/Controllers/User/VideoController.php
@@ -35,7 +35,7 @@ class VideoController extends Controller
                 'videos' => function ($query) use ($filters) {
                     $query
                         ->filter($filters)
-                        ->with('category:id,title')
+                        ->with(['category:id,title', 'user:id'])
                         ->withCount(['likes', 'dislikes', 'interactions', 'comments', 'views'])
                         ->latest('updated_at');
                 }
