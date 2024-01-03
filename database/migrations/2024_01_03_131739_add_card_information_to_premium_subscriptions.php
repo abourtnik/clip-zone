239717054ba5f28093124523bf25ff8d6ac07bff
abourtnik <contact@antonbourtnik.fr>
2024-01-03 15:12:55 +0100

    fix : migration

--- database/migrations/2024_01_03_131739_add_card_information_to_premium_subscriptions.php
+++ database/migrations/2024_01_03_131739_add_card_information_to_premium_subscriptions.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up() :void
     {
         Schema::table('premium_subscriptions', function (Blueprint $table) {
-            $table->after('trials_end_at', function (Blueprint $table) {
+            $table->after('trial_ends_at', function (Blueprint $table) {
                 $table->string('card_last4')->after('trials_end_at');
                 $table->date('card_expired_at')->after('trials_end_at');
             });
