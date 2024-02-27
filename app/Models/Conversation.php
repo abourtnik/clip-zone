b57dec15854bb0a29e2d98e4aed7f561d254c93f
abourtnik <contact@antonbourtnik.fr>
2024-02-27 14:14:46 +0100

    feat : add migrations and models

---
+++ app/Models/Conversation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin IdeHelperPlan
+ */
+class Conversation extends Model
+{
+    protected $guarded = ['id'];
+}
