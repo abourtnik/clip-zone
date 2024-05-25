0257fea63b03dc060787b87820f092bf71ff0958
abourtnik <contact@antonbourtnik.fr>
2024-05-25 19:54:32 +0200

    Fix tests

--- database/migrations/2022_10_30_172213_create_videos_table.php
+++ database/migrations/2022_10_30_172213_create_videos_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->string('file');
             $table->string('original_file_name');
             $table->string('mimetype');
+            $table->string('thumbnail')->nullable();
             $table->unsignedMediumInteger('duration');
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->foreignIdFor(Category::class)->nullable()->constrained();
