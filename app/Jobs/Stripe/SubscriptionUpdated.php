3d719fa9bbb8f0c000f76cb7c24416f8a1ab8e88
abourtnik <contact@antonbourtnik.fr>
2025-03-24 16:30:17 +0100

    Upgrade laravel to 11.44.1

--- app/Jobs/Stripe/SubscriptionUpdated.php
+++ app/Jobs/Stripe/SubscriptionUpdated.php
@@ -9,7 +9,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Stripe\Subscription as StripeSubscription;
 use Throwable;
 
 class SubscriptionUpdated implements ShouldQueue
