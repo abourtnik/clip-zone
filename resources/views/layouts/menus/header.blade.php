d483ab8f8b756103771b029e18a5f527fbdfed2d
abourtnik <contact@antonbourtnik.fr>
2026-03-05 18:36:13 +0100

    Add [x-cloak] for prevent alpine js blip

--- resources/views/layouts/menus/header.blade.php
+++ resources/views/layouts/menus/header.blade.php
@@ -1,5 +1,5 @@
 <nav @class(["navbar bg-white border border-start-0 px-2 sticky-top header", 'navbar-expand-md' => $show_sidebar])  x-data="{search:false}">
-    <div class="container-fluid" x-show.important="!search">
+    <div class="container-fluid" x-show.important="!search" x-cloak>
         <div @class(["gap-3 d-flex align-items-center justify-content-start", "gap-md-0 justify-content-md-center" => $show_sidebar ])>
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#responsive-sidebar">
                 <span class="navbar-toggler-icon" style="width: 1.2em;height: 1.2em;"></span>
@@ -44,7 +44,7 @@
                 @endauth
         </ul>
     </div>
-    <div class="d-flex w-100 gap-3 px-2" x-show.important="search">
+    <div class="d-flex w-100 gap-3 px-2" x-show.important="search" x-cloak>
         <button @click="search = false" class="nav-link d-flex align-items-center bg-transparent">
             <i  class="fa-solid fa-arrow-left"></i>
         </button>
